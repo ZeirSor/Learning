13938ba6c5fffa143261a4a52df928df7304ca55
ZeirSor <Szpsn2357@gmail.com>
2022-10-24 10:37:43 +0800

    Update PrintWriter_.java

--- javaProject/heima_Learning/IO-demo/src/print/PrintWriter_.java
+++ javaProject/heima_Learning/IO-demo/src/print/PrintWriter_.java
@@ -7,8 +7,11 @@ import java.io.PrintWriter;
 public class PrintWriter_ {
     public static void main(String[] args) throws IOException {
         String destOut = "C:\\Users\\ASUS\\Desktop\\printWriter.txt";
+
         PrintWriter printWriter = new PrintWriter(new FileWriter(destOut));
+
         printWriter.println("hello world");
+        
         printWriter.close();        //记得close 否则写不进文件
     }
 }
